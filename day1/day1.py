33d5450a6d5ec4cbd5ab8b35cd05a27199c6589f
Cory Berg <cberg18@gmail.com>
2020-12-01 21:51:55 -0700

    day 1, part 2

--- day1/day1.py
+++ day1/day1.py
@@ -3,7 +3,8 @@ data = [int(line.strip()) for line in open("/home/cberg18/Documents/git/adventOf
 def check(input):
     for i in input:
         for j in input:
-            if i + j == 2020:
-                return i*j
+            for k in input:
+                if i + j + k == 2020:
+                    return i*j*k
 
 print(check(data))
